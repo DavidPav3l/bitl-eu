9d04df03f69ddcda7e1143ab9acd5cadc1330cd8
DavidPav3l <david_marian@outlook.com>
2024-03-23 07:57:04 +0200

    bla bla

--- app/(client)/blog/page.jsx
+++ app/(client)/blog/page.jsx
@@ -10,12 +10,13 @@ export const metadata = {
 };
 
 async function getPosts() {
-  const query = `*[_type == "post"] {
+  const query = `*[_type == "post"] | order(publishedAt desc) {
     title,
     slug,
     author->{name},
     publishedAt,
-    body
+    body,
+    excerpt
   }`;
   const posts = await client.fetch(query);
   return posts;
@@ -27,18 +28,26 @@ function BlogPostPreview({ post }) {
   return (
     <Link
       href={`/blog/${post.slug.current}`}
-      className="space-y-8 rounded-lg border px-6 py-4 lg:px-10 lg:py-6"
+      className="rounded-lg border px-2 py-4 sm:space-y-8 sm:px-6 lg:px-10 lg:py-6"
     >
-      <h2 className="text-lg font-bold xs:text-xl lg:text-2xl">{post.title}</h2>
+      <div className="space-y-4">
+        <h2 className="text-center text-xl font-bold xs:text-2xl lg:text-2xl">
+          {post.title}
+        </h2>
+
+        <p className="text-sm xs:text-base lg:text-lg">{post.excerpt}</p>
+      </div>
 
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <User className="h-8 w-8" />
-          <span>{post.author.name}</span>{" "}
+        <div className="flex flex-col items-center gap-3 border border-dashed p-2 sm:flex-row">
+          <User className="h-6 w-6 sm:h-8 sm:w-8" />
+          <span className="text-center">{post.author.name}</span>{" "}
         </div>
-        <div className="flex items-center gap-3">
-          <CalendarDays className="h-8 w-8" />
-          <p>{new Date(post.publishedAt).toDateString()}</p>
+        <div className="flex flex-col items-center gap-3 border border-dashed p-2 sm:flex-row">
+          <CalendarDays className="h-6 w-6 sm:h-8 sm:w-8" />
+          <p className="text-center">
+            {new Date(post.publishedAt).toDateString()}
+          </p>
         </div>
       </div>
     </Link>
@@ -49,8 +58,8 @@ export default async function BlogPostsPage() {
   const posts = await getPosts();
 
   return (
-    <main className="mx-auto w-[calc(90%_-_16px)] max-w-screen-xl">
-      <div className="mt-20 space-y-20">
+    <main className="mx-auto w-[calc(95%_-_8px)] max-w-screen-xl">
+      <div className="mt-20 space-y-10 sm:space-y-14 lg:space-y-20">
         <h1 className="text-center text-2xl font-bold xs:text-3xl sm:text-4xl lg:text-5xl">
           Blog Posts
         </h1>
